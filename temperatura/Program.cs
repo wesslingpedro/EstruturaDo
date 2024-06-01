322a66983ce2f0c57baf93877f77f6a715839569
wesslingpedro <wesslingpedro@gmail.com>
2024-06-01 10:36:30 -0300

    Primeiro commit

---
+++ temperatura/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace testes_ec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double C, F;
+            char repetir;
+
+            do
+            {
+                Console.Write("Digite a temperatura em Celsius: ");
+                C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                F = 9.0 * C / 5.0 + 32.0;
+                Console.WriteLine("Equivalente em Fahreinheit: " + F.ToString("F1", CultureInfo.InvariantCulture));
+                Console.Write("Deseja repetir (s/n)? ");
+                repetir = char.Parse(Console.ReadLine());
+            } while (repetir == 's');
+
+
+
+
+            Console.ReadLine();
+        }
+
+    }
+
+}
